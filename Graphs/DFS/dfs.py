dcee8e08b2d118551d8cb904b309f4f426f491fd
Vortex <51107910+agrawalmalav@users.noreply.github.com>
2023-03-19 13:15:18 +0530

    Create dfs.py

---
+++ Graphs/DFS/dfs.py
@@ -0,0 +1,50 @@
+class Graph:
+    def __init__(self, nVertices):
+        self.nVertices = nVertices
+        self.adjMat = [[0 for i in range(nVertices)] for j in range(nVertices)]
+    def addEdge(self, v1, v2):
+        self.adjMat[v1][v2] = 1
+        self.adjMat[v2][v1] = 1
+        return
+    
+    def checkEdge(self, v1, v2):
+        return True if self.adjMat[v1][v2] != 0 else False
+        
+    def delEdge(self, v1, v2):
+        if self.checkEdge( v1, v2):
+            self.adjMat[v1][v2] = 0
+            self.adjMat[v2][v1] = 0
+        return
+    
+    def dfsHelper(self, sv, visited):
+        print(sv)
+        visited[sv] = True
+        for i in range(self.nVertices):
+            if (self.adjMat[sv][i] >0 and visited[i] == False):
+                self.dfsHelper(i, visited)
+        
+                
+        
+    def dfs(self):
+        visited = [False for i in range(self.nVertices)]
+        self.dfsHelper(0, visited)
+        
+    
+    def __str__(self):
+        return (str(self.adjMat))
+        
+        
+# main
+g = Graph(5)
+g.addEdge(0, 1)
+g.addEdge(1, 3)
+g.addEdge(2, 4)
+g.addEdge(2, 3)
+g.addEdge(2, 0)
+
+
+
+g.dfs()
+
+print(g)
+    

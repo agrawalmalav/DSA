512edeb2586eb1cea91835aac08034719e524060
Vortex <51107910+agrawalmalav@users.noreply.github.com>
2023-04-23 23:39:19 +0530

    Create next_largest.py

---
+++ Trees/Generic Trees/Next larger/next_largest.py
@@ -0,0 +1,50 @@
+from sys import stdin,setrecursionlimit
+setrecursionlimit(10**6)
+class treeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+def nextLargest(tree, n):
+    #############################
+    # PLEASE ADD YOUR CODE HERE #
+    #############################
+    ans = None
+    if tree.data > n:
+        if ans == None or ans.data > tree.data:
+            ans = tree
+
+    for child in tree.children:
+        node = nextLargest(child, n)
+        if node != None and (ans == None or node.data < ans.data):
+             ans=node
+        #elif node != None and ans != None and node.data < ans.data:
+        #    ans = node
+    return  ans
+
+
+
+
+
+def createLevelWiseTree(arr):
+    root = treeNode(int(arr[0]))
+    q = [root]
+    size = len(arr)
+    i = 1
+    while i<size:
+        parent = q.pop(0)
+        childCount = int(arr[i])
+        i += 1
+        for j in range(0,childCount):
+            temp = treeNode(int(arr[i+j]))
+            parent.children.append(temp)
+            q.append(temp)
+        i += childCount
+    return root
+
+# Main
+arr = list(int(x) for x in stdin.readline().strip().split())
+n = int(input())
+tree = createLevelWiseTree(arr)
+if nextLargest(tree, n):
+    print(nextLargest(tree, n).data)

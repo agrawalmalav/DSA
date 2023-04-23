cf94ace2fa4214bb1b8e8b2fda530f895099eeca
Vortex <51107910+agrawalmalav@users.noreply.github.com>
2023-04-23 23:36:42 +0530

    Create identical_generic_trees.py

---
+++ Trees/Generic Trees/Structurally identical/identical_generic_trees.py
@@ -0,0 +1,52 @@
+from sys import stdin,setrecursionlimit
+setrecursionlimit(10**6)
+class treeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+def isIdentical(tree1, tree2):
+    #############################
+    # PLEASE ADD YOUR CODE HERE #
+    #############################
+    #print(tree1.data, tree2.data)
+    if tree1.data != tree2.data or len(tree1.children) !=len(tree2.children):
+        #print('from 1st if')
+        return False
+    
+    for x in range(len(tree1.children)):
+        if not isIdentical(tree1.children[x], tree2.children[x]):
+            #print('from loop')
+            return False
+    return True
+
+
+
+
+
+
+def createLevelWiseTree(arr):
+    root = treeNode(int(arr[0]))
+    q = [root]
+    size = len(arr)
+    i = 1
+    while i<size:
+        parent = q.pop(0)
+        childCount = int(arr[i])
+        i += 1
+        for j in range(0,childCount):
+            temp = treeNode(int(arr[i+j]))
+            parent.children.append(temp)
+            q.append(temp)
+        i += childCount
+    return root
+
+# Main
+arr1 = list(int(x) for x in stdin.readline().strip().split())
+tree1 = createLevelWiseTree(arr1)
+arr2 = list(int(x) for x in stdin.readline().strip().split())
+tree2 = createLevelWiseTree(arr2)
+if isIdentical(tree1, tree2):
+    print('true')
+else:
+    print('false')

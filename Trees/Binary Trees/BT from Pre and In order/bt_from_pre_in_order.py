e37bf6e009b179cc3eca7b67df67cc5d933c37ac
Vortex <51107910+agrawalmalav@users.noreply.github.com>
2023-07-25 19:33:51 +0530

    Create bt_from_pre_in_order.py

---
+++ Trees/Binary Trees/BT from Pre and In order/bt_from_pre_in_order.py
@@ -0,0 +1,81 @@
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+
+#Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+
+def buildTree(preOrder, inOrder, n) :
+	#Your code goes here
+    if n ==0:
+        return
+    if n ==1:
+        return BinaryTreeNode(preOrder[0])
+
+    rootData = preOrder[0]
+    idx = inOrder.index(rootData)
+    root = BinaryTreeNode(rootData)
+
+    leftTree = buildTree(preOrder[1:idx+1] , inOrder[:idx], idx )
+    rightTree = buildTree(preOrder[idx+1:], inOrder[idx+1:], len(inOrder) - idx -1)
+
+    root.left = leftTree
+    root.right = rightTree
+    return root
+
+
+def printLevelWise(root):
+    if root is None :
+        return
+
+    pendingNodes = queue.Queue()
+    pendingNodes.put(root)
+    pendingNodes.put(None)
+
+    while not pendingNodes.empty(): 
+        frontNode = pendingNodes.get()
+    
+        if frontNode is None :
+            print()
+            
+            if not pendingNodes.empty() :
+                pendingNodes.put(None)
+                
+        else :
+            print(frontNode.data, end = " ")
+            
+            if frontNode.left is not None :
+                pendingNodes.put(frontNode.left)
+                
+                
+            if frontNode.right is not None :
+                pendingNodes.put(frontNode.right)
+
+
+                
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    n = int(stdin.readline().strip())
+
+    if n == 0 :
+        return list(), list(), 0
+
+    preOrder = list(map(int, stdin.readline().strip().split(" ")))
+    inOrder = list(map(int, stdin.readline().strip().split(" ")))
+
+    return preOrder, inOrder, n
+
+
+# Main
+preOrder, inOrder, n = takeInput()
+root = buildTree(preOrder, inOrder, n)
+printLevelWise(root)

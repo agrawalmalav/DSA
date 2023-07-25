93c834d6c10e24ff95b5c01a49b6f0e24cfba9f1
Vortex <51107910+agrawalmalav@users.noreply.github.com>
2023-07-25 22:17:47 +0530

    Create reverse_LL.py

---
+++ Linked Lists/Reverse LL/reverse_LL.py
@@ -0,0 +1,101 @@
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10 ** 6)
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+def reverseLinkedListRec(head) :
+	#Your code goes here
+    if head == None or head.next == None:
+        return head
+
+    tail = head.next
+    newHead = reverseLinkedListRec(head.next)
+    tail.next = head
+    head.next = None
+    return newHead
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+
+
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    head = takeInput()
+
+    newHead = reverseLinkedListRec(head)
+    printLinkedList(newHead)
+
+    t -= 1

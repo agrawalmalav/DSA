18e9120e0c185144595af47673da42f8303e2b93
Vortex <51107910+agrawalmalav@users.noreply.github.com>
2023-07-25 22:25:58 +0530

    Create midpoint_of_linked_list.py

---
+++ Linked Lists/Midpoint of Linked List/midpoint_of_linked_list.py
@@ -0,0 +1,84 @@
+from sys import stdin
+
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+def midPoint(head) :
+    # Write your code here
+    if head == None or head.next == None:
+        return head
+    slow , fast = head, head
+
+    while fast.next != None and fast.next.next != None:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+# Main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    head = takeInput()
+    mid = midPoint(head)
+
+    if mid is not None :
+        print(mid.data)
+
+    t -= 1

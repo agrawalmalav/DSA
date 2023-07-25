fb484f6ea669e1b2f83d6283198eaf509ae4ad1b
Vortex <51107910+agrawalmalav@users.noreply.github.com>
2023-07-25 22:37:51 +0530

    Create kReverse.py

---
+++ Linked Lists/kReverse/kReverse.py
@@ -0,0 +1,104 @@
+from sys import stdin
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+def kReverse(head, k) :
+	#Your code goes here
+    if k==0 or head== None or head.next == None:
+        return head
+    curr, prev, next = head, None, None
+    cnt = 0
+    while curr is not None and cnt < k:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+        cnt += 1
+
+    if next is not None:
+        head.next = kReverse(next, k)
+
+    return prev
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+
+
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    head = takeInput()
+    k = int(stdin.readline().strip())
+
+    newHead = kReverse(head, k)
+    printLinkedList(newHead)
+
+    t -= 1

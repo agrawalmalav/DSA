0b29607c815a55cd759c57f02b11eed0ffe96b3f
Vortex <51107910+agrawalmalav@users.noreply.github.com>
2023-08-13 23:13:46 +0530

    Create rotate_array.py

---
+++ Arrays and Lists/Rotate Array/rotate_array.py
@@ -0,0 +1,39 @@
+from sys import stdin
+
+
+def rotate(arr, n, d):
+    #Your code goes here
+    temp = arr[:d]
+    arr[:n-d] = arr[d:]
+    arr[n-d:] = temp
+
+
+
+#Taking Input Using Fats I/O
+def takeInput() :
+    n = int(stdin.readline().rstrip())
+    if n == 0:
+        return list(), 0
+
+    arr = list(map(int, stdin.readline().rstrip().split(" ")))
+    return arr, n
+
+
+#to print the array/list 
+def printList(arr, n) : 
+    for i in range(n) :
+        print(arr[i], end = " ")
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    arr, n = takeInput()
+    d = int(stdin.readline().rstrip())
+    rotate(arr, n, d)
+    printList(arr, n)
+    
+    t -= 1
